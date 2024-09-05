fdf2739b0917cee7ab5b4e208dd13527d6eeeba7
Arinji2 <arinjaydhar205@gmail.com>
2024-09-05 11:13:10 +0530

    Refactor Errors for is Update Uses

--- tasks/helpers/pocketbase/update-uses.go
+++ tasks/helpers/pocketbase/update-uses.go
@@ -5,15 +5,11 @@ import (
 
 	"github.com/Arinji2/vibeify-backend/api"
 	"github.com/Arinji2/vibeify-backend/types"
+	user_errors "github.com/Arinji2/vibeify-backend/user-errors"
 )
 
-func UpdateUses(user *types.PocketbaseUser, used int, usesID string) (errorText string) {
-	adminToken, err := GetPocketbaseAdminToken()
-	errorText = "Server Error"
-	if err != "" {
-		fmt.Println(err)
-		return
-	}
+func UpdateUses(user *types.PocketbaseUser, used int, usesID string) error {
+	adminToken := GetPocketbaseAdminToken()
 
 	client := api.NewApiClient()
 	_, _, error := client.SendRequestWithBody("PATCH", fmt.Sprintf("/api/collections/convertLimit/records/%s", usesID), map[string]string{
@@ -24,11 +20,9 @@ func UpdateUses(user *types.PocketbaseUser, used int, usesID string) (errorText 
 	})
 
 	if error != nil {
-		fmt.Println(error)
-		return
+
+		return user_errors.NewUserError("", error)
 	}
 
-	errorText = ""
-
-	return
+	return nil
 }
